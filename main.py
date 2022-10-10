11e9a3684299d526466a292c719d3b96d5a923a1
LUCAS LEMOS MONTEIRO MONTEIRO <lucas02@carneiro.local>
2022-10-10 13:52:15 -0300

    sei la

--- main.py
+++ main.py
@@ -34,17 +34,19 @@ def create_positives(image_path):
     lab = cv2.cvtColor(img_crp, cv2.COLOR_BGR2LAB)
     for i in range(1, 10, 3):
         l_channel, a, b = cv2.split(lab)
-        clp = 1.0*i - 4
+        clp = -10.0*i - 4
         tgs = (i, i)
         clahe = cv2.createCLAHE(clipLimit=clp, tileGridSize=tgs)
         cl = clahe.apply(l_channel)
         limg = cv2.merge((cl, a, b))
         enhanced_img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
         result = cv2.cvtColor(enhanced_img, cv2.COLOR_BGR2GRAY)
-        img_pos.append(result)
+        gray = cv2.cvtColor(img_crp, cv2.COLOR_BGR2GRAY)
+        a = np.hstack((gray, result))
+        img_pos.append(a)
     # Brilho
     hsv = cv2.cvtColor(img_crp, cv2.COLOR_BGR2HSV)
-    for i in range(10, 50, 10):
+    for i in range(10, 100, 30):
         h, s, v = cv2.split(hsv)
         lim = 255 - i
         v[v > lim] = 255
@@ -52,7 +54,9 @@ def create_positives(image_path):
         final_hsv = cv2.merge((h, s, v))
         enhanced_img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
         result = cv2.cvtColor(enhanced_img, cv2.COLOR_BGR2GRAY)
-        img_pos.append(result)
+        gray = cv2.cvtColor(img_crp, cv2.COLOR_BGR2GRAY)
+        a = np.hstack((gray, result))
+        img_pos.append(a)
     return img_pos
 
 
@@ -64,15 +68,33 @@ def show_image(image, gray=True):
     plt.show()
 
 
+def minhaFunc(image):
+    m = np.ones(image.shape, dtype="uint8") * 100
+    added = cv2.add(image, m)
+    return added
+
+def minhaFunc2(image):
+    m = np.ones(image.shape, dtype="uint8") * 100
+    added = cv2.subtract(image, m)
+    return added
+
+
 def main():
     images_path = load_paths()
+    cont = 0
+    value = random.randint(0, 256)
     for image_path in images_path:
-        img_ach = load_image(image_path)
-        img_pos = create_positives(image_path)
-        show_image(img_ach)
-        for image in img_pos:
-            show_image(image)
-        break
+        print(cont)
+        print(value)
+        if(cont == value):
+            img_ach = load_image(image_path)
+            img_pos = create_positives(image_path)
+            img_pos = minhaFunc(img_ach)
+            img_pos2 = minhaFunc2(img_ach)
+            img = np.hstack((img_ach, img_pos))
+            show_image(np.hstack((img, img_pos2)))
+            break
+        cont+=1
 
 
 if __name__ == '__main__':
